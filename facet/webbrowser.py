9ed13930950eef821bd425b0c930c6d7b10047bb
Dan Davison <dandavison7@gmail.com>
2016-04-30 04:08:48 -0700

    Focus chrome tab

--- facet/webbrowser.py
+++ facet/webbrowser.py
@@ -1,12 +1,22 @@
 import re
 import subprocess
 import webbrowser
+from textwrap import dedent
 
 
-class ChromeCli:
-    TAB_REGEX = re.compile(
-        r'^\[(?P<window_id>\d+):(?P<tab_id>\d+)\] +(?P<content>.+)'
-    )
+class Chrome:
+    # Example line of `chrome-cli list tabs` output:
+    # [1794:2294] python parsimonious - Google Search
+    TAB_REGEX = re.compile(r'''
+    ^
+    \[
+      (?P<window_id>\d+)
+      :
+      (?P<tab_id>\d+)
+    \]
+    \s+
+    (?P<content>.+)
+    ''', re.VERBOSE)
 
     def activate(self, tab_id):
         self._run('activate', '-t', tab_id)
@@ -32,18 +42,55 @@ class ChromeCli:
     def _run(self, *commands):
         return subprocess.check_output(('chrome-cli',) + commands)
 
+    def get_tab_and_window_index(self, tabs, predicate):
+        """
+        Return first parsed tab matching predicate, and index of tab's window.
+
+        Input is a list of tab dicts (as returned by `list_tabs` or
+        `list_links`, and a tab predicate function. The sequence of window IDs
+        in the tab list is assumed to reflect the window index recognized by
+        the OS (Window Manager). I.e., a list of tabs
+
+        [{'window_id': 1634}, {'window_id': 1634}, {'window_id': 1748}]
+
+        would be assumed to represent two tabs with window index 1 and one tab
+        with window index 2.
+        """
+        window_index = 0
+        window_id = None
+        for tab in tabs:
+            if tab['window_id'] != window_id:
+                window_index += 1
+                window_id = tab['window_id']
+            if predicate(tab):
+                return tab, window_index
+        raise StopIteration
+
+    def raise_tab(self, tab, window_index):
+        """
+        Activate tab and raise tab's window.
+        """
+        self.activate(tab['tab_id'])
+        script = dedent("""
+        tell application "System Events" to tell process "Google Chrome"
+            perform action "AXRaise" of window {window_index}
+            set frontmost to true
+        end tell
+        """).format(window_index=window_index)
+        proc = subprocess.Popen(['osascript', '-'], stdin=subprocess.PIPE)
+        stdout, stderr = proc.communicate(script.encode('utf-8'))
+        assert not stderr, stderr
+
 
 def open_url(url):
-    chrome_cli = ChromeCli()
+    chrome = Chrome()
+    tabs = chrome.list_links()
     try:
-        tab = next(
-            tab
-            for tab in chrome_cli.list_links()
-            if tab['content'] == url)
+        tab, window_index = chrome.get_tab_and_window_index(
+            tabs,
+            lambda tab: tab['content'] == url,
+        )
     except StopIteration:
-        return webbrowser.open(url)
+        webbrowser.open(url)
     else:
-        chrome_cli.activate(tab['tab_id'])
-        subprocess.check_call([
-            'osascript', '-e', 'activate application "Google Chrome"'
-        ])
+        chrome.raise_tab(tab, window_index)
