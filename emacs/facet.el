fa66a29ff200fcecb07e1ea7531902c32d080425
Dan Davison <dandavison7@gmail.com>
2016-07-17 12:54:13 -0700

    Process facet output more sophisticatedly

--- emacs/facet.el
+++ emacs/facet.el
@@ -4,6 +4,9 @@
 ;; Keywords: project, task
 ;; URL: https://github.com/dandavison/facet
 
+(require 's)
+(require 'helm)
+
 (defvar facet-directory (expand-file-name "~/.facet/"))
 
 ;;;###autoload
@@ -38,10 +41,19 @@
   (dired (expand-file-name (facet-current-facet) (facet-facets-directory))))
 
 (defun facet-list ()
-  (mapcar
-   (lambda (line) (let* ((words (split-string line)))
-               `(,line . ,(first words))))
-   (split-string (shell-command-to-string "facet ls") "[\n\r]+")))
+  (let* ((rows
+          (mapcar
+           (lambda (line) (split-string line "\t"))
+           (split-string (s-chomp (shell-command-to-string "facet ls")) "\n")))
+         (left-column-width
+          (apply #'max (mapcar (lambda (row) (length (car row))) rows)))
+         (left-column-fmt (format "%%-%ds %%s" left-column-width)))
+    (mapcar
+     (lambda (row)
+       `(,(format left-column-fmt (car row) (or (cadr row) ""))
+         .
+         ,(car row)))
+     rows)))
 
 (defun facet-current-facet ()
   (cdr (assoc 'facet (facet-read-state))))
